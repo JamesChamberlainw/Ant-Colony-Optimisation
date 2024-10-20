28dc7a387eba16fcc804aefcf5f96cbae936b7a9
James William Chamberlain <jw.chamberlain@outlook.com>
2024-10-20 06:33:08 +0100

    cleaning code
    added comments

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-import numpy as np 
-import matplotlib.pyplot as plt
-import itertools
+# file created by James William Chamberlain on 2024-10-24 06:30:00 GMT 
+
 import csv
+import numpy as np 
 import pandas as pd
 
+
 def load_data():
+    """
+    Load in BankProblem.txt, sort text file into useable dataframe
+    
+    returns: pd.DataFrame with columns 'weight' and 'value'
+    """
     df = pd.DataFrame(columns=['weight', 'value'])
 
     with open('BankProblem.txt','r') as f:
         reader = csv.reader(f, delimiter = '|')
         rows = list(reader)
         rows.pop(0) # drop the first row (header)
-        # select next three rows
 
         while rows != []:
             rows.pop(0) # drop bag number as its not needed
@@ -21,7 +26,14 @@ def load_data():
             rows.pop(0) # drop value
             new_df = pd.DataFrame([[weight, value]], columns=['weight', 'value'])
             df = pd.concat([df, new_df], ignore_index=False)
-
+             
     return df
 
-df = load_data()
+
+
+
+# initialize the dataset 
+df = load_data() 
+
+
+
